c8b364be08edfc8cf2705042db78a6572639a6a8
Benjamin Berkowitz <bjberkow@gmail.com>
2023-11-18 18:02:56 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# QuizProject
-Timed Quiz
+AS A lifelong fan of Star Wars
+I WANT to take a timed quiz on Star Wars
+that stores high scores and will be fun to partake in
+SO THAT I can gauge my progress compared to my peers
---
+++ questions.js
@@ -0,0 +1,54 @@
+// Question Array 
+var questions = [
+    {
+      title: "The working title for Star Wars was Blue Harvest.",
+      choices: ["True", "False"],
+      answer: "True"
+      <Audio 
+    },
+    {
+      title: "What line of dialogue has been spoken in every film?",
+      choices: [
+        "I will not be the last jedi.",
+        "I am one with the force, the force is with me.",
+        "I've got a bad feeling about this.",
+        "I have lost the Star Wars."
+      ],
+      answer: "I've got a bad feeling about this."
+    },
+    {
+      title: "Who was the first character to say the line I have a bad feeling about this?",
+      choices: [
+        "Luke Skywalker",
+        "Han Solo",
+        "R2-D2",
+        "Obi-Wan Kenobi"
+      ],
+      answer: "Luke Skywalker"
+    },
+    {
+      title:
+        "Which character said the I have a bad feeling about this line in The Last Jedi",
+      choices: [
+        "Poe Dameron",
+        "C-3P0",
+        "Kylo Ren",
+        "BB-8"
+      ],
+      answer: "BB-8"
+    },
+    {
+      title: "In Rogue One, what was the nickname given to Jyn Erso by her father?",
+      choices: [
+        "Stardust", 
+        "Star-Lord", 
+        "Starkiller", 
+        "Starman"],
+      answer: "Stardust"
+    },
+    {
+      title: "C-3P0 is fluent in over 60 million languages.",
+      choices: ["True", "False"],
+      answer: "True"
+    }
+  ];
